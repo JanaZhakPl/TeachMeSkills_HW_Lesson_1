62bc7d2d3f1988d7673008ad4d9dc9f4e640c9b3
JanaZhak <93000882+JanaZhakPl@users.noreply.github.com>
2021-12-01 22:42:27 +0100

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " +  name);
+    }
+}
